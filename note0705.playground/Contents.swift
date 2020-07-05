08f7cd6524fe4bdbe88ef89d8249b340938a2a21
Lydia-Lu-code <guipopo5@gmail.com>
2020-07-05 22:52:45 +0800

    selectSander-1

---
+++ note0705.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+var select1WoM = "man"
+var select2Sex = "sameSex"
+var select3H = ""
+var height: Int = 159
+var select4W = ""
+var weight: Int = 0
+var select5Skill = ""
+var skillWord = ""
+var select6Cash = ""
+var cash: Int = 0
+var result = ""
+var sender = 0
+
+
+if
